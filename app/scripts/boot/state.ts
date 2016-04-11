6f49a1370185eec92d60fb7d76cf7ae115e953e9
Sean Spradlin <seanspradlin@gmail.com>
2016-04-10 20:00:53 -0500

    force scaling

--- app/scripts/boot/state.ts
+++ app/scripts/boot/state.ts
@@ -5,12 +5,15 @@ namespace Boot {
   export class State extends Phaser.State {
     preload() {
       this.game.scale.pageAlignHorizontally = true;
+      this.game.scale.pageAlignVertically = true;
       this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
       this.game.stage.backgroundColor = 0x88c070;
     }
     
     create() {
       console.log('Boot complete');
+      this.game.scale.maxWidth = this.game.width;
+      this.game.scale.maxHeight = this.game.height;
       this.game.stage.smoothed = false;
       this.game.state.start('Loader');
     }
