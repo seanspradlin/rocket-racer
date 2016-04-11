c13c94c01c7b2d23a07f03b018a9635ae6a33f10
Sean Spradlin <seanspradlin@gmail.com>
2016-04-10 21:59:21 -0500

    camera follows player correctly

--- app/scripts/main/state.ts
+++ app/scripts/main/state.ts
@@ -9,10 +9,15 @@ namespace Main {
       this.physics.startSystem(Phaser.Physics.ARCADE);
       this.time.advancedTiming = true;
       
-      this.player = new Player(this, this.world.width /2, this.world.height /2);
-      new Platform(this, -32, this.stage.height - 24, this.stage.width + 64, 24);
-      new Platform(this, 60, this.stage.height - 300, 128);
-      new Platform(this, 120, this.stage.height - 700, 256);
+      this.world.setBounds(0, 0, this.game.width, this.game.height * 2);
+      
+      this.player = new Player(this, this.world.width /2, this.world.bounds.bottom  - 56);
+      new Platform(this, -32, this.world.bounds.bottom - 24, this.stage.width + 64, 24);
+      new Platform(this, 60, this.world.bounds.bottom - 300, 128);
+      new Platform(this, 120, this.world.bounds.bottom - 700, 256);
+      
+      this.camera.follow(this.player);
+      this.camera.deadzone = new Phaser.Rectangle(0, 168, 640, 800);
       console.log('Game Started');
     }
     
