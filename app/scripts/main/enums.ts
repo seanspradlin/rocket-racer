cf498e8be42ae50cad7c5b75fe426c95d23ab798
Sean Spradlin <seanspradlin@gmail.com>
2016-04-11 20:08:26 -0500

    created enums

---
+++ app/scripts/main/enums.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+'use strict';
+
+namespace Main {
+  export enum PlatformSurfaceTypes {
+    STATIC,
+    CONVEYOR,
+    ICY,
+    SPRINGY
+  }
+  
+  export enum PlatformMotionTypes {
+    STATIONARY,
+    MOVING
+  }
+}
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -4,6 +4,7 @@
 /// <reference path="../app/scripts/main/state.ts" />
 /// <reference path="../app/scripts/main/player.ts" />
 /// <reference path="../app/scripts/main/platform.ts" />
+/// <reference path="../app/scripts/main/platform-generator.ts" />
 /// <reference path="../app/scripts/menu/state.ts" />
 /// <reference path="../app/scripts/loader/state.ts" />
 /// <reference path="../app/scripts/index.ts" />
