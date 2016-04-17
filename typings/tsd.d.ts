29fef86aa95751b08fe9c9e4cf1a3ead67de3dee
Sean Spradlin <seanspradlin@gmail.com>
2016-04-16 19:05:55 -0500

    add controls to tsd

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -5,6 +5,7 @@
 /// <reference path="../app/scripts/main/enums.ts" />
 /// <reference path="../app/scripts/main/interfaces.ts" />
 /// <reference path="../app/scripts/main/state.ts" />
+/// <reference path="../app/scripts/main/controls.ts" />
 /// <reference path="../app/scripts/main/player.ts" />
 /// <reference path="../app/scripts/main/platform.ts" />
 /// <reference path="../app/scripts/menu/state.ts" />
